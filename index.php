ad1192951f16d182e5bedec13c357f36f7530063
Wortar <hugo.crach@my-digital-school.org>
2018-01-18 14:43:31 +0100

    Fusionnn

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html>
+	<head>
+	
+		<meta charset="utf-8">
+		<title>Document sans nom</title>
+		
+		<link rel="stylesheet" href="vendors/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+		
+	</head>
+
+	<body>
+		
+		<div class="produitmenu">
+		
+			<?php include "parts/header.php" ?>
+			
+			<div class="block-center">
+				<div class="inner">
+				
+					<section class="accroche" >
+							<h1 class="title">
+							Produits
+							</h1>
+
+							<h2 class="definition">
+								Une déferlante de produits pour des soirées enflammées: des boissons, décorations, gadgets, ballons... et plein d'autres choses pour vous satisfaire!
+							</h2 >
+					</section>
+					
+					<nav class="nav-center">
+					
+						<ul>
+						
+							<li><a href="#" class="apero">Apéro - boissons</a></li>
+							
+							<li><a href="#" class="deco">Décoration</a></li>
+							
+							<li><a href="#" class="accessoires">Accessoires</a></li>
+							
+							<li><a href="#" lass="table">table</a></li>
+							
+						</ul>
+						
+					</nav>						
+					
+				</div>
+			</div>
+				
+			<?php include "parts/footer.php" ?>
+			
+		</div>
+	
+	</body>
+	
+</html>
